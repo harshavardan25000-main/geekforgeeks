d180934d1d0ac4f368393654b391b2925ed79db0
harshavardan25000-main <harsha.manam07@gmail.com>
2020-12-17 11:20:52 -0600

    array rotation with block reversal

---
+++ Arrays/arrayRot_reverseBlock.py
@@ -0,0 +1,13 @@
+arr=[1,2,3,4,5,6,7,8,9]
+d=2
+l=len(arr)
+def reverse(arr,start,end):
+    while start<end:
+        arr[start],arr[end]=arr[end],arr[start]
+        start,end=start+1,end-1
+def rotation(arr,d,l):
+    reverse(arr,0,d-1)
+    reverse(arr,d,l-1)
+    reverse(arr,0,l-1)
+rotation(arr,d,l)
+print(arr)
