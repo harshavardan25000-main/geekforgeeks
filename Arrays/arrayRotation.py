88303af480fd79d7881e6df7dc46bf0185e89c36
harshavardan25000-main <harsha.manam07@gmail.com>
2020-12-16 22:17:59 -0600

    juggling method

--- Arrays/arrayRotation.py
+++ Arrays/arrayRotation.py
@@ -1,7 +1,17 @@
-arr=[1,2,3,4,5,6,7]
+import math
+arr=[1,2,3,4,5,6,7,8,9]
+d=2
+l=len(arr)
+gcd_n=math.gcd(d,l)
 #arr=None
-def rotate(arr,d):
+def rotate(arr,d,l,gcd_n):
     if not arr or len(arr)<d:
         return None
-    return arr[d:]+arr[:d]
-print(rotate(arr,2))
+    for i in range(gcd_n):
+        temp=arr[i]
+        for i in range(i,l-gcd_n,gcd_n):
+            arr[i]=arr[i+gcd_n]
+        arr[i+gcd_n]=temp
+rotate(arr,d,l,gcd_n)
+
+print(arr)
